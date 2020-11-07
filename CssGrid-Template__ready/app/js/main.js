b81c3235a69ffda16879b784fa24acca17c169f3
Yaroslav Shilov <workyarik@gmail.com>
2020-11-07 11:01:03 +0300

    CssGrid-Template ready

---
+++ CssGrid-Template__ready/app/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+});//End Ready
+
+
+//BEGIN copyright
+//let copyYear = new Date();
+//document.getElementById('copy').innerHTML = copyYear.getFullYear()
+//END copyright
